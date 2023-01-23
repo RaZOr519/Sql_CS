2078f4763ccd94f567a1fd455bab718598aacb4e
Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
2023-01-23 09:07:21 +0530

    Update Q01.cs

--- Q01.cs
+++ Q01.cs
@@ -9,3 +9,28 @@ cmd.Parameters.AddWithValue("@Gender", Gender);
 cmd.ExcuteNonQuery();
 
 connection.Close();
+
+//2
+
+connection.Open();
+            //SqlCommand cmd = new SqlCommand(@"SELECT * FROM [Table] WHERE CID='" + id.Text + "'", connection);
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = (@"SELECT * FROM [Table] WHERE cid='" + textBox1.Text + "'");
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                textBox2.Text = reader[2].ToString();
+                textBox3.Text = reader[3].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Not Found");
+            }
+            connection.Close();
+        }
+
+//3
